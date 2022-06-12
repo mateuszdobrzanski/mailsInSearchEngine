ded93d08b2101aed7d7c5a2326ecb9075890df22
mdob <mateuszdobrzanski@outlook.com>
2022-06-12 20:23:48 +0200

    added try catch amd skip tiktok page

--- mailsGoogleSearchEngine.py
+++ mailsGoogleSearchEngine.py
@@ -3,6 +3,8 @@ import time
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from googlesearch import search
 import requests
+import ssl
+import urllib.request
 
 requests.packages.urllib3.disable_warnings()
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
@@ -13,8 +15,16 @@ except AttributeError:
     pass
 
 
-def emails_by_domain(domain):
+class TLSAdapter(requests.adapters.HTTPAdapter):
 
+    def init_poolmanager(self, *args, **kwargs):
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        kwargs['ssl_context'] = ctx
+        return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
+
+
+def emails_by_domain(domain):
     # to search
     query = "@" + domain
 
@@ -23,14 +33,43 @@ def emails_by_domain(domain):
 
     final_list = []
 
-    for j in search(query, tld="pl", num=10, stop=10, pause=5):
-        page = requests.get(j, verify=False)
-        get_emails = re.findall(EMAIL_REGEX, page.text)
-        cleaned_emails = list(dict.fromkeys(get_emails))
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+    from urllib.request import Request, urlopen
+    import certifi
+    import ssl
+
+    for j in search(query, tld="pl", num=10, stop=30, pause=5):
+        session = requests.session()
+        session.mount('https://', TLSAdapter())
+
+        req = urllib.request.Request(j, headers=header)
+
+        cleaned_emails = []
+        message = ""
+
+        if "tiktok" in j:
+            message = "Error, weird page"
+        else:
+            try:
+                response = urllib.request.urlopen(req, context=ssl.create_default_context(cafile=certifi.where()))
+                page = response.read().decode('utf-8')
+
+                get_emails = re.findall(EMAIL_REGEX, page)
+                cleaned_emails = list(dict.fromkeys(get_emails))
+
+            except Exception as ex:
+                template = "An exception of type {0} occurred."
+                message = template.format(type(ex).__name__)
+                print(message)
+
         dictionary = {
             "page": j,
-            "emails": cleaned_emails
+            "emails": cleaned_emails,
+            "error": message
         }
         final_list.append(dictionary)
+
+        time.sleep(5)
 
     return final_list
